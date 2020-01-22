f5de7b8fd238d5c58360aab5269d0b998718b76d
Moid Shams <me_moid@hotmail.com>
2020-01-22 19:18:45 +0500

    added

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -87,25 +87,25 @@ namespace WebApplication2
             services.AddScoped<IMatchService, MatchService>();
             services.AddScoped<IRoleRightRepository, RoleRightRepository>();
         }
-    }
+    
 
 
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseDeveloperExceptionPage();
         }
+        else
+        {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseMvc();
     }
 }
+    }
