b974a73c6fe23bc0053ee6abc534cd2562fa5106
Moid Shams <me_moid@hotmail.com>
2020-02-01 14:39:14 +0500

    TeamPlayersController added

---
+++ BLL.Service/Services/TeamPlayersService.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Entities;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Service.Services
+{
+   
+        public class TeamPlayersService : BaseService<TeamPlayers, int>, ITeamPlayersService
+    {
+            public TeamPlayersService(RequestScope scopeContext, ITeamPlayerRepository repository)
+                : base(scopeContext, repository)
+            {
+
+            }
+        }
+        public interface ITeamPlayersService : IBaseService<TeamPlayers, int>
+        {
+
+        }
+    
+}
---
+++ WebApplication2/Controllers/TeamPlayersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Service.Services;
+using DAL.Entities;
+using DAL.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+   
+    public class TeamPlayersController : BaseController<TeamPlayers,int>
+    {
+        public TeamPlayersController(RequestScope scopeContext,ITeamPlayersService service)
+            :base(scopeContext,service)
+        {
+
+        }
+    }
+}
